b85de3f7c917c1385d1c6b1fb70987cf2ce61e38
dyyyl <hey@dyyyl.rocks>
2023-04-24 15:27:58 -0400

    metadata go brrrr

--- missing-persons-viewer-vite/src/App/components/Header/Header.tsx
+++ missing-persons-viewer-vite/src/App/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import { Semibold } from 'src/shared/components/Typography';
+import metadata from 'src/shared/data/meta.json';
 
 export const Header = () => {
-  const latestDataUpdated = '2021-09-01';
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(new Date(metadata.updated_at));
+  const latestDataUpdated = formattedDate || '2021-09-01';
 
   return (
     <header>
